2a916814ad91832a57c95f9ecb37ab34f1ef438b
shane <spp935@gmail.com>
2017-07-18 11:47:37 +1000

    Delete types

--- typings/typescript.d.ts
+++ typings/typescript.d.ts
@@ -1,7 +1,4 @@
 import {SpecificShallowEqualType} from "../src/createSpecificShallowEqual"
-import {Iterable, List, Map} from "immutable";
-import {Moment} from "moment";
-import {Action} from "redux";
 
 declare module "cs.core"{
     export function createSpecificShallowEqual<TProps> (...keysToTest: (keyof TProps)[]) : (currentProps: SpecificShallowEqualType<TProps>, nextProps: SpecificShallowEqualType<TProps>) => boolean
